430b5f383ea2cf7e3d3bc4ca3e56abf9a2da586b
portalBlock <portalBlock@fakeEmail.com>
2014-02-23 13:37:02 -0600

    Mixing it up :D

--- src/main/java/net/portalblock/creativeblockz/worlds/MemberWorld.java
+++ src/main/java/net/portalblock/creativeblockz/worlds/MemberWorld.java
@@ -13,27 +13,23 @@ import java.io.File;
  */
 public class MemberWorld {
     public MemberWorld(){
-        if(CreativeBlockz.instance.getServer().getWorld("memberWorld") != null){
-            CreativeBlockz.instance.getLogger().info("memberWorld Loaded, no need to go further.");
+        if(CreativeBlockz.instance.getServer().getWorld("defaultBlockz") != null){
+            CreativeBlockz.instance.getLogger().info("defaultBlockz is already loaded, why is this even being called???");
             return;
         }
 
-        File dir = new File("/");
-        for(File file : dir.listFiles()){
-            if(file.getName().equals("memberWorld")&&file.isDirectory()){
-                CreativeBlockz.instance.getLogger().info("memberWorld exists :D I will create an instance now.");
-                WorldCreator wc = new WorldCreator("memberWorld");
-                wc.generator(new PlotGen(PlotManager.getMap("memberWorld")));
-                wc.environment(World.Environment.NORMAL);
-                CreativeBlockz.memberWorld = wc.createWorld();
-                return;
-            }
-        }
+        CreativeBlockz.instance.getLogger().info("Creating instance of defaultBlockz now :D");
+        WorldCreator worldCreator = new WorldCreator("defaultBlockz");
+        worldCreator.generator(new PlotGen(PlotManager.getMap("defaultBlockz")));
+        worldCreator.environment(World.Environment.NORMAL);
+        worldCreator.generateStructures(false);
+        World world = worldCreator.createWorld();
+        world.setSpawnFlags(false, false);
+        world.setPVP(false);
+        world.setStorm(false);
+        world.setThundering(false);
+        world.setAutoSave(true);
 
-        CreativeBlockz.instance.getLogger().info("memberWorld doesn't exist, I will make one now :D");
-        WorldCreator wc = new WorldCreator("memberWorld");
-        wc.generator(new PlotGen(PlotManager.getMap("memberWorld")));
-        wc.environment(World.Environment.NORMAL);
-        CreativeBlockz.memberWorld = wc.createWorld();
+        CreativeBlockz.instance.memberWorld = world;
     }
 }
